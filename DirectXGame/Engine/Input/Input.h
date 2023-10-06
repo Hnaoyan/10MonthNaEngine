5a440f8843a29fb0c2903b6d99fc3c0852eab41f
Hnaoyan <k022g0041@g.neec.ac.jp>
2023-10-06 11:20:09 +0900

    コメントアウト追加

--- DirectXGame/Engine/Input/Input.h
+++ DirectXGame/Engine/Input/Input.h
@@ -88,18 +88,36 @@ public:	// メンバ関数
 public:	// コントローラー系
 	
 	/// <summary>
-	/// 
+	/// 今フレームのコントローラーの状態
 	/// </summary>
 	/// <param name="stickNo"></param>
 	/// <param name="out"></param>
 	/// <returns></returns>
 	bool GetJoystickState(int32_t stickNo, DIJOYSTATE2& out);
 
-	bool GetJoystickStatePrevious(int32_t stickNo, DIJOYSTATE2& out);
+	/// <summary>
+	/// 前フレームのコントローラーの状態
+	/// </summary>
+	/// <param name="stickNo"></param>
+	/// <param name="out"></param>
+	/// <returns></returns>
+	bool GetJoystickStatePrev(int32_t stickNo, DIJOYSTATE2& out);
 
+	/// <summary>
+	/// 今フレームのコントローラーの状態
+	/// </summary>
+	/// <param name="stickNo"></param>
+	/// <param name="out"></param>
+	/// <returns></returns>
 	bool GetJoystickState(int32_t stickNo, XINPUT_STATE& out);
 
-	bool GetJoystickStatePrevious(int32_t stickNo, XINPUT_STATE& out);
+	/// <summary>
+	/// 前フレームのコントローラーの状態
+	/// </summary>
+	/// <param name="stickNo"></param>
+	/// <param name="out"></param>
+	/// <returns></returns>
+	bool GetJoystickStatePrev(int32_t stickNo, XINPUT_STATE& out);
 
 	void SetJoystickDeadZone(int32_t stickNo, int32_t deadZoneL, int32_t deadZoneR);
 
