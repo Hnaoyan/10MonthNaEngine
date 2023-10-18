20caecd2e57ece27a5271b4f64422efe6c4fb47d
野村優羽 <@MSI>
2023-10-18 18:10:27 +0900

    アニメーションの秒数を一度一秒に

--- DirectXGame/Application/Others/AnimationManager/AnimationManager.cpp
+++ DirectXGame/Application/Others/AnimationManager/AnimationManager.cpp
@@ -7,7 +7,7 @@ void AnimationManager::Initialize()
 	actionAnimationTimer_ = 0;
 
 	// 行動アニメーションタイム
-	actionAnimationTime_ = 20;
+	actionAnimationTime_ = 1;
 
 	// 行動アニメーションしているか
 	isActionAnimation_ = false;
