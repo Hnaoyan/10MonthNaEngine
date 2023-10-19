f6d2c943090bd71ff9380c181381e3197a1e550a
野村優羽 <@MSI>
2023-10-19 16:53:22 +0900

    アニメーションクラス修正

--- DirectXGame/Application/Others/AnimationManager/AnimationManager.cpp
+++ DirectXGame/Application/Others/AnimationManager/AnimationManager.cpp
@@ -3,14 +3,14 @@
 void AnimationManager::Initialize()
 {
 
-	// 行動アニメーションタイマー
-	actionAnimationTimer_ = 0;
+	// 行動アニメーション
+	actionAnimation_.Initialize();
 
-	// 行動アニメーションタイム
-	actionAnimationTime_ = 20;
+	// ゲームクリアアニメーション
+	gameClearAnimation_.Initialize();
 
-	// 行動アニメーションしているか
-	isActionAnimation_ = false;
+	// ゲームオーバーアニメーション
+	gameOverAnimation_.Initialize();
 
 }
 
@@ -26,16 +26,16 @@ void AnimationManager::WaitUpdate()
 void AnimationManager::ActionUpdate()
 {
 
-	for (std::function<void()> function : actionAnimations_) {
+	for (std::function<void()> function : actionAnimation_.animations_) {
 		function();
 	}
 
 	WaitUpdate();
 
-	actionAnimationTimer_++;
-	if (actionAnimationTimer_ == actionAnimationTime_) {
-		isActionAnimation_ = false;
-		actionAnimations_.clear();
+	actionAnimation_.animationTimer_++;
+	if (actionAnimation_.animationTimer_ == actionAnimation_.animationTime_) {
+		actionAnimation_.isAnimation_ = false;
+		actionAnimation_.animations_.clear();
 	}
 
 }
@@ -43,8 +43,54 @@ void AnimationManager::ActionUpdate()
 void AnimationManager::ActionStart()
 {
 
-	isActionAnimation_ = true;
-	actionAnimationTimer_ = 0;
+	actionAnimation_.isAnimation_ = true;
+	actionAnimation_.animationTimer_ = 0;
+
+}
+
+void AnimationManager::GameClearUpdate()
+{
+
+	for (std::function<void()> function : gameClearAnimation_.animations_) {
+		function();
+	}
+
+	gameClearAnimation_.animationTimer_++;
+	if (gameClearAnimation_.animationTimer_ == gameClearAnimation_.animationTime_) {
+		gameClearAnimation_.isAnimation_ = false;
+		gameClearAnimation_.animations_.clear();
+	}
+
+}
+
+void AnimationManager::GameClearStart()
+{
+
+	gameClearAnimation_.isAnimation_ = true;
+	gameClearAnimation_.animationTimer_ = 0;
+
+}
+
+void AnimationManager::GameOverUpdate()
+{
+
+	for (std::function<void()> function : gameOverAnimation_.animations_) {
+		function();
+	}
+
+	gameOverAnimation_.animationTimer_++;
+	if (gameOverAnimation_.animationTimer_ == gameOverAnimation_.animationTime_) {
+		gameOverAnimation_.isAnimation_ = false;
+		gameOverAnimation_.animations_.clear();
+	}
+
+}
+
+void AnimationManager::GameOverStart()
+{
+
+	gameOverAnimation_.isAnimation_ = true;
+	gameOverAnimation_.animationTimer_ = 0;
 
 }
 
@@ -54,14 +100,9 @@ void AnimationManager::Reset()
 	// 待機アニメーション関数リスト
 	waitingAnimations_.clear();
 
-	// 行動アニメーション関数リスト
-	actionAnimations_.clear();
-
-	// 行動アニメーションタイマー
-	actionAnimationTimer_ = 0;
-
-	// 行動アニメーションしているか
-	isActionAnimation_ = false;
+	actionAnimation_.Reset();
+	gameClearAnimation_.Reset();
+	gameOverAnimation_.Reset();
 
 }
 
@@ -75,6 +116,43 @@ void AnimationManager::SetWaitingAnimation(std::function<void()> function)
 void AnimationManager::SetActionAnimation(std::function<void()> function)
 {
 
-	actionAnimations_.push_back(function);
+	actionAnimation_.animations_.push_back(function);
+
+}
+
+void AnimationManager::SetGameClearAnimation(std::function<void()> function)
+{
+
+	gameClearAnimation_.animations_.push_back(function);
+
+}
+
+void AnimationManager::SetGameOverAnimation(std::function<void()> function)
+{
+
+	gameOverAnimation_.animations_.push_back(function);
+
+}
+
+void AnimationManager::AnimationStruct::Initialize()
+{
+
+	// 行動アニメーションタイマー
+	animationTimer_ = 0;
+	// 行動アニメーションタイム
+	animationTime_ = 20;
+	// 行動アニメーションしているか
+	isAnimation_ = false;
+
+}
+
+void AnimationManager::AnimationStruct::Reset()
+{
+	// 行動アニメーション関数リスト
+	animations_.clear();
+	// 行動アニメーションタイマー
+	animationTimer_ = 0;
+	// 行動アニメーションしているか
+	isAnimation_ = false;
 
 }
--- DirectXGame/Application/Others/AnimationManager/AnimationManager.h
+++ DirectXGame/Application/Others/AnimationManager/AnimationManager.h
@@ -7,6 +7,29 @@
 /// </summary>
 class AnimationManager
 {
+public: // サブクラス
+	// アニメーション構造体
+	struct AnimationStruct
+	{
+		// 行動アニメーション関数リスト
+		std::list<std::function<void()>> animations_;
+
+		// 行動アニメーションタイマー
+		uint32_t animationTimer_;
+
+		// 行動アニメーションタイム
+		uint32_t animationTime_;
+
+		// 行動アニメーションしているか
+		bool isAnimation_;
+
+		// 初期化
+		void Initialize();
+		
+		// リセット関数
+		void Reset();
+	};
+
 
 public: // メンバ関数
 
@@ -31,6 +54,26 @@ public: // メンバ関数
 	void ActionStart();
 
 	/// <summary>
+	/// ゲームクリア時更新処理
+	/// </summary>
+	void GameClearUpdate();
+
+	/// <summary>
+	/// ゲームクリア開始時設定
+	/// </summary>
+	void GameClearStart();
+
+	/// <summary>
+	/// ゲームオーバー時更新処理
+	/// </summary>
+	void GameOverUpdate();
+
+	/// <summary>
+	/// ゲームオーバー開始時設定
+	/// </summary>
+	void GameOverStart();
+
+	/// <summary>
 	/// リセット
 	/// </summary>
 	void Reset();
@@ -47,27 +90,42 @@ public: // メンバ関数
 	/// <param name="function">関数</param>
 	void SetActionAnimation(std::function<void()> function);
 
+	/// <summary>
+	/// ゲームクリアアニメーション関数リスト追加
+	/// </summary>
+	/// <param name="function">関数</param>
+	void SetGameClearAnimation(std::function<void()> function);
+
+	/// <summary>
+	/// ゲームオーバーアニメーション関数リスト追加
+	/// </summary>
+	/// <param name="function">関数</param>
+	void SetGameOverAnimation(std::function<void()> function);
+
 public: // アクセッサ
 
 	// 行動アニメーションしているか
-	bool GetIsActionAnimation() { return isActionAnimation_; }
+	bool GetIsActionAnimation() { return actionAnimation_.isAnimation_; }
+
+	// ゲームクリアアニメーションしているか
+	bool GetIsGameClearAnimation() { return gameClearAnimation_.isAnimation_; }
+
+	// ゲームオーバーアニメーションしているか
+	bool GetIsGameOverAnimation() { return gameOverAnimation_.isAnimation_; }
 
 private: // メンバ変数
 
 	// 待機アニメーション関数リスト
 	std::list<std::function<void()>> waitingAnimations_;
 
-	// 行動アニメーション関数リスト
-	std::list<std::function<void()>> actionAnimations_;
-	
-	// 行動アニメーションタイマー
-	uint32_t actionAnimationTimer_;
+	// 行動アニメーション
+	AnimationStruct actionAnimation_;
 
-	// 行動アニメーションタイム
-	uint32_t actionAnimationTime_;
+	// ゲームクリアアニメーション
+	AnimationStruct gameClearAnimation_;
 
-	// 行動アニメーションしているか
-	bool isActionAnimation_;
+	// ゲームオーバーアニメーション
+	AnimationStruct gameOverAnimation_;
 
 };
 
