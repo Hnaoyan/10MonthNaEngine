393ea1a0792ab5bdb6cf6d4a72ef5a0bd3d7772a
Hnaoyan <k022g0041@g.neec.ac.jp>
2023-10-20 18:26:18 +0900

    サイズ調整

--- DirectXGame/Application/UI/StageNumberUI/StageNumberUI.cpp
+++ DirectXGame/Application/UI/StageNumberUI/StageNumberUI.cpp
@@ -4,12 +4,12 @@ void StageNumberUI::Initialize(uint32_t texture)
 {
 	Vector4 color = { 1.0f,1.0f,1.0f,1.0f };
 	Vector2 anchorPoint = { 0,0 };
-	position_ = { 950.0f,10.0f };
+	position_ = { 1000.0f,10.0f };
 	size_ = { 300.0f,128.0f };
 
 	textureHandle_ = texture;
 	stageNumberSprite_.reset(Sprite::Create(textureHandle_, position_, color, anchorPoint, false, false));
-	stageNumberSprite_->SetSize(size_);
+	stageNumberSprite_->SetSize({ size_.x - 50.0f,size_.y });
 	stageNumberSprite_->SetSpriteRect(Vector2{ 0,0 }, size_);
 	stageNumberSprite_->SetBlendMode(Sprite::BlendMode::kNormal);
 	stageNumberSprite_->Update();
